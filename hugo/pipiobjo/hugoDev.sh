02111986e3cdb6918d6d2a9d8ef0811ee155ac8e
Björn Pipiorke <prodyna.pipiorke@extaccount.com>
2020-05-24 23:35:02 +0200

    providing rss feed, adding new post

--- hugo/pipiobjo/hugoDev.sh
+++ hugo/pipiobjo/hugoDev.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+DIR=$(pwd)
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # make the file an executable (only once)
-chmod +x ./asciidoctor
+chmod +x $SCRIPT_DIR/asciidoctor
+
+cd $SCRIPT_DIR
+
 # calling hugo in dev mode
-PATH=$PWD:$PATH hugo server -D --debug
+hugo server -D --debug -b http://localhost:1313
+
+# switching back 
+cd $DIR
